41eec81cb9b72e4aa9df593f10e0005888af2693
Alexandra <aestalx@aim.com>
2018-03-20 08:41:15 -0400

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+         has_many :tweets
+         serialize :following, Array
+         ####look me at pdf26
+
+         validates :username, presence: true
+         validates :username, uniqueness: true
+end
